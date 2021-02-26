e75137a7c47b444f3ea3fd7c5215671bb2dc3518
Takahiko Wada <wada.takahiko@gmail.com>
2021-02-27 07:53:34 +0900

    add lottie

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,55 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React, { useRef } from "react";
+import { Button, StyleSheet, View, Image, Text } from "react-native";
+import LottieView from "lottie-react-native";
 
 export default function TabOneScreen() {
+  const animationRef = useRef<LottieView | null>();
+
+  const resetAnimation = () => {
+    if (animationRef.current) {
+      animationRef.current.reset();
+      animationRef.current.play();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={styles.animationContainer}>
+      <Text style={styles.text}>Lottie</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Restart Animation" onPress={resetAnimation} />
+      </View>
+      <LottieView
+        ref={(animation) => {
+          animationRef.current = animation;
+        }}
+        style={styles.image}
+        source={require("../assets/animations/cycling.json")}
+      />
+      <Text style={styles.text}>Animation Gif</Text>
+      <Image
+        style={styles.image}
+        source={require("../assets/animations/cycling.gif")}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
+  buttonContainer: {
+    paddingTop: 20,
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  text: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontWeight: "bold",
   },
 });
